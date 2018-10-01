eb6679513ffb1815aca8cfdc7832f13c07e2e0a9
Jantastication O <georginaoregx@gmail.com>
2018-10-01 21:28:47 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -16,11 +16,11 @@ def first_element(array)
   array[0]
 end
 
-def first_element(array)
+def third_element(array)
   array[3]
 end
 
-def first_element(array)
+def last_element(array)
   array[-1]
 end
 
