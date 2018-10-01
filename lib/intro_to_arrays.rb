73e65f91e3e88d05ca3f306d8791d816943a83cf
Jantastication O <georginaoregx@gmail.com>
2018-10-01 21:18:54 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,27 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["monday", "tuesday"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+array[3]
+
+array[-1]
+
+def first_element_with_array_methods(array)
+  array.unshift
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
